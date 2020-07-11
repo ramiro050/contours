fbe165cea0a7cce247302dc39992f4f2e984b609
ramiro050 <ramiroleal050@gmail.com>
2020-07-11 10:54:38 -0500

    Added unit tests for flooding

--- src/main.rs
+++ src/main.rs
@@ -4,87 +4,322 @@
 //! of contours in a 2D map.
 
 use peroxide::prelude::*;
-use std::collections::VecDeque;
+use std::collections::{VecDeque, HashSet};
+use std::cmp::Ordering::{self, Less, Greater};
 mod point;
 use point::{Point};
+use gnuplot::{Figure};
 
 fn main() {
-    let a = py_matrix(vec![
-        vec![1, 2],
-        vec![3, 4]]);
+    let a = match Matrix::read("../data/sampleMap.csv", false, ',') {
+        Ok(m) => Terrain::new(m),
+        _ => panic!("Unable to read csv")
+    };
+    let mut flood_reg = Region::zeros_like(&a.matrix);
+    let mut boundary = Boundary::new(vec![Point::origin()]);
 
-    a.print();
+    let h = -0.22553230822086334;
+    let (_, new_boundary_pts) = flood(&a, &mut flood_reg, &boundary, h);
 
-    let mut ft = zeros(2, 2);
-    let sp = vec![Point::origin()];
+    boundary.pts = new_boundary_pts;
+
+    println!("Boundary len: {}", boundary.pts.len());
+
+    let (lower_h, upper_h) = find_saddle(&a, &mut flood_reg, &boundary);
+
+    println!("Found heights: {:?}, {:?}", lower_h, upper_h);
+
+    let mut fig = Figure::new();
+    fig
+        .axes2d()
+        .image(flood_reg.matrix.data, flood_reg.matrix.row,
+               flood_reg.matrix.col, None, &[]);
+
+    //fig.show();
 }
 
-struct Region {
-    region: Matrix,
-    boundary: Vec<Point>
+#[derive(Copy, Clone)]
+pub enum Curvature {Pos, Neg}
+
+pub struct Terrain {
+    matrix: Matrix,
+    maxs: Vec<Point>,
+    mins: Vec<Point>
 }
 
-impl Region {
-    fn is_flooded_at(&self, p: Point) -> bool {
-        self.region[p.tuple()] > 0.0
+impl Terrain {
+    pub fn new(m: Matrix) -> Terrain {
+        Terrain {
+            matrix: m,
+            maxs: Vec::new(),
+            mins: Vec::new()
+        }
+    }
+
+    pub fn zeros_like(m: &Matrix) -> Terrain {
+        Terrain{
+            matrix: zeros(m.row, m.col),
+            maxs: Vec::new(),
+            mins: Vec::new()
+        }
+    }
+
+    pub fn shape(&self) -> (usize, usize) {
+        (self.matrix.row, self.matrix.col)
+    }
+
+    pub fn can_flood_at(&self, p: Point, height_bound: f64,
+                        curvature: Curvature) -> bool {
+        let terrain_val = self.matrix[p.tuple()];
+
+        match curvature {
+            Curvature::Pos => terrain_val >= height_bound,
+            Curvature::Neg => terrain_val < height_bound
+        }
     }
 }
 
-fn flood<F>(terrain: &Matrix, flood_reg: &mut Region, can_flood: F) -> Vec<Point>
-where F: Fn(&Matrix, Point) -> bool {
-    let flood_reg_shape = (flood_reg.region.row, flood_reg.region.col);
-    assert_eq!((terrain.row, terrain.col), flood_reg_shape);
+pub struct Boundary {
+    pts: Vec<Point>,
+    curvature: Curvature
+}
 
-    let max_pt = Point(terrain.row, terrain.col);
+impl Boundary {
+    pub fn new(pts: Vec<Point>) -> Boundary {
+        Boundary {
+            pts: pts,
+            curvature: Curvature::Pos
+        }
+    }
+}
 
-    let mut queue: VecDeque<Point> = flood_reg.boundary
-        .iter()
-        .flat_map(|&x| x.get_adjacent_pts())
-        .filter(|&x| x.is_in_rect(Point::origin(), max_pt)
-                && flood_reg.is_flooded_at(x))
-        .collect();
+pub struct Region {
+    matrix: Matrix
+}
 
+impl Region {
+    pub fn new(m: Matrix) -> Region {
+        Region { matrix: m }
+    }
+
+    pub fn zeros_like(m: &Matrix) -> Region {
+        Region{ matrix: zeros(m.row, m.col) }
+    }
+
+    pub fn is_flooded_at(&self, p: Point) -> bool {
+        self.matrix[p.tuple()] > 0.0
+    }
+
+    pub fn flood_at(&mut self, p: Point) {
+        self.matrix[p.tuple()] = 1.0;
+    }
+
+    pub fn unflood_at(&mut self, p: Point) {
+        self.matrix[p.tuple()] = 0.0;
+    }
+
+    pub fn shape(&self) -> (usize, usize) {
+        (self.matrix.row, self.matrix.col)
+    }
+
+    pub fn is_pt_in_region(&self, p: Point) -> bool {
+        p.is_in_rect(Point::origin(), Point(self.matrix.row, self.matrix.col))
+    }
+}
+
+pub fn flood(terrain: &Terrain, flood_reg: &mut Region,
+             boundary: &Boundary, h: f64) -> (Vec<Point>, Vec<Point>) {
+    assert_eq!(terrain.shape(), flood_reg.shape());
+
+    let mut queue: VecDeque<Point> = VecDeque::new();
     let mut flooded_pts: Vec<Point> =  Vec::new();
-    let mut new_boundary: Vec<Point> = Vec::new();
+    let mut new_boundary_pts: Vec<Point> = Vec::new();
+
+    for &boundary_pt in &boundary.pts {
+        if terrain.can_flood_at(boundary_pt, h, boundary.curvature) {
+            flood_reg.flood_at(boundary_pt);
+            flooded_pts.push(boundary_pt);
+            queue.push_back(boundary_pt);
+        } else {
+            new_boundary_pts.push(boundary_pt);
+        }
+    }
 
     while !queue.is_empty() {
-        let curr_pt = queue.pop_front().unwrap_or(Point::origin());
+        let curr_pt = queue.pop_front().unwrap();
         let adj_pts: Vec<Point> = curr_pt
             .get_adjacent_pts()
             .into_iter()
-            .filter(|&x| x.is_in_rect(Point::origin(), max_pt)
+            .filter(|&x| flood_reg.is_pt_in_region(x)
                     && !flood_reg.is_flooded_at(x))
             .collect();
 
         for adj_pt in adj_pts {
-            if can_flood(terrain, adj_pt) {
-                flood_reg.region[adj_pt.tuple()] = 1.0;
-
+            if terrain.can_flood_at(adj_pt, h, boundary.curvature) {
+                flood_reg.flood_at(adj_pt);
                 flooded_pts.push(adj_pt);
                 queue.push_back(adj_pt);
             } else {
-                new_boundary.push(adj_pt);
+                new_boundary_pts.push(adj_pt);
             }
         }
     }
 
-    flood_reg.boundary = new_boundary;
-    return flooded_pts;
+    return (flooded_pts, new_boundary_pts);
+}
+
+pub fn trace_boundary(flood_reg: &Region, start: Point) -> HashSet<Point> {
+    let mut visited: HashSet<Point> = HashSet::new();
+    if flood_reg.is_flooded_at(start) { return visited }
+
+    let mut queue: VecDeque<Point> = VecDeque::new();
+
+    if start
+        .get_adjacent_pts()
+        .into_iter()
+        .any(|x| flood_reg.is_pt_in_region(x)
+             && flood_reg.is_flooded_at(x)) {
+            visited.insert(start);
+            queue.push_back(start);
+    } else {
+        return visited;
+    }
+
+    while !queue.is_empty() {
+        let curr_pt = queue.pop_front().unwrap();
+        let adj_pts: Vec<Point> = curr_pt
+            .get_adjacent_pts()
+            .into_iter()
+            .filter(|&x| !visited.contains(&x)
+                    && flood_reg.is_pt_in_region(x)
+                    && !flood_reg.is_flooded_at(x)
+                    && x
+                    .get_adjacent_pts()
+                    .into_iter()
+                    .any(|x| flood_reg.is_pt_in_region(x)
+                         && flood_reg.is_flooded_at(x)))
+            .collect();
+
+        for adj_pt in adj_pts {
+            visited.insert(adj_pt);
+            queue.push_back(adj_pt);
+        }
+    }
+
+    visited
+}
+
+pub fn cmp(terrain: &Terrain, flood_reg: &mut Region,
+           boundary: &Boundary, h: f64) -> Ordering {
+    let (flooded_pts, new_boundary_pts) = flood(terrain, flood_reg, boundary, h);
+
+    let extrema = match boundary.curvature {
+        Curvature::Pos => &terrain.maxs,
+        Curvature::Neg => &terrain.mins
+    };
+
+    if extrema
+        .into_iter()
+        .any(|&x| flood_reg.is_flooded_at(x)) {
+            for pt in flooded_pts {
+                flood_reg.unflood_at(pt);
+            }
+            return Greater;
+        }
+
+    let first_pt = match new_boundary_pts.first() {
+        Some(&first) => first,
+        None => panic!() // Deal with this without crashing
+    };
+
+    let circle = trace_boundary(flood_reg, first_pt);
+    if circle.len() > new_boundary_pts.len() {
+        for pt in flooded_pts {
+            flood_reg.unflood_at(pt);
+        }
+        return Greater;
+    }
+
+    return Less;
+}
+
+fn increasing<T>(x: &T, y: &T) -> Ordering where T: PartialOrd {
+    x.partial_cmp(y).unwrap()
+}
+
+fn decreasing<T>(x: &T, y: &T) -> Ordering where T: PartialOrd {
+    y.partial_cmp(x).unwrap()
+}
+
+pub fn find_saddle(terrain: &Terrain, flood_reg: &mut Region,
+                   boundary: &Boundary) -> (Option<f64>, Option<f64>) {
+    let sort_fn: fn(&f64, &f64) -> Ordering =
+        match boundary.curvature {
+            Curvature::Pos => decreasing,
+            Curvature::Neg => increasing
+        };
+
+    let mut heights: Vec<f64> = (&boundary.pts)
+        .into_iter()
+        .map(|p| terrain.matrix[p.tuple()])
+        .collect();
+
+    heights.sort_by(sort_fn);
+
+    let cmp_fn = |&h| cmp(terrain, flood_reg, boundary, h);
+    let h_index = match heights.binary_search_by(cmp_fn) {
+        Err(i) => i,
+        _      => panic!("Comparison function returned equality!")
+    };
+
+    let upper_h = if h_index == heights.len() {None} else {Some(heights[h_index])};
+    let lower_h = if h_index == 0 {None} else {Some(heights[h_index - 1])};
+
+    (lower_h, upper_h)
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn eye() {
-        let mut a = super::zeros(10, 10);
-        a[(0, 0)] = 1.0;
-        let boundary = vec![super::Point(0, 0)];
+    use quickcheck as qc;
 
-        let mut region = super::Region { region: a, boundary: boundary};
+    qc::quickcheck! {
+        fn prop_flood_all(i: usize, j: usize) -> bool {
+            let start = super::Point(i % 50, j % 50);
+            let region = super::zeros(50, 50);
+            let mut flood_reg = super::Region::new(region);
 
-        let terrain = super::py_matrix(vec![vec![1; 10]; 10]);
+            let terrain = super::Terrain::zeros_like(&flood_reg.matrix);
 
-        super::flood(&terrain, &mut region, |_, _| true);
-        assert_eq!(1, 1);
+            let mut boundary = super::Boundary::new(vec![start]);
+            boundary.curvature = super::Curvature::Neg;
+
+            super::flood(&terrain, &mut flood_reg, &boundary, 100.0);
+
+            flood_reg.matrix.data.into_iter().all(|x| x == 1.0)
+        }
+
+        fn prop_trace_line(i: usize) -> bool {
+            let mut region = super::zeros(50, 50);
+            for j in 0..50 {
+                region[(i % 50, j)] = 1.0;
+            }
+
+            let flood_reg = super::Region::new(region);
+
+            let start: super::Point;
+            if i % 50 == 0 { start = super::Point(i % 50 + 1, 0) }
+            else { start = super::Point(i % 50 - 1, 0) }
+
+            super::trace_boundary(&flood_reg, start).len() == 50
+        }
+
+        fn prop_trace_no_boundary(i: usize, j: usize) -> bool {
+            let start = super::Point(i % 50, j % 50);
+            let region = super::zeros(50, 50);
+
+            let flood_reg = super::Region::new(region);
+
+            super::trace_boundary(&flood_reg, start).len() == 0
+        }
     }
 }
--- src/point.rs
+++ src/point.rs
@@ -1,36 +1,63 @@
-use std::ops::{Add, Sub};
+//! Point
+//!
+//! The following defines a structure and a set of methods and functions
+//! to operate on 2D coordinates.
 
-#[derive(PartialEq, Copy, Clone, Debug)]
+use std::ops::Add;
+use std::convert::identity;
+
+/// Structure representing 2D array indexes
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
 pub struct Point(pub usize, pub usize);
 impl Point {
+    /// Initializes a `Point` at the origin
     pub fn origin() -> Point {
         Point(0, 0)
     }
 
-    pub fn right(&self) -> Point {
-        Point(self.0 + 1, self.1)
+    /// Create a new point to the right of `self`
+    pub fn right(&self) -> Option<Point> {
+        Some(Point(self.0 + 1, self.1))
     }
 
-    pub fn left(&self) -> Point {
-        Point(self.0 - 1, self.1)
+    /// Create a new point to the left of `self`. If the coordinates become
+    /// negative, then `None` is returned.
+    pub fn left(&self) -> Option<Point> {
+        if self.0 == 0 { None }
+        else { Some(Point(self.0 - 1, self.1)) }
     }
 
-    pub fn up(&self) -> Point {
-        Point(self.0, self.1 + 1)
+    /// Create a new point above `self`
+    pub fn up(&self) -> Option<Point> {
+        Some(Point(self.0, self.1 + 1))
     }
 
-    pub fn down(&self) -> Point {
-        Point(self.0, self.1 - 1)
+    /// Create a new point below `self`. If the coordinates become
+    /// negative, then `None` is returned.
+    pub fn down(&self) -> Option<Point> {
+        if self.1 == 0 { None }
+        else { Some(Point(self.0, self.1 - 1)) }
     }
 
+    /// Turns `Point` into a tuple
     pub fn tuple(&self) -> (usize, usize) {
         (self.0, self.1)
     }
 
+    /// Gets a vector of all the points adjacent to `self` that do
+    /// not have negative coordinates
     pub fn get_adjacent_pts(&self) -> Vec<Point> {
         vec![self.right(), self.left(), self.up(), self.down()]
+            .into_iter()
+            .filter_map(identity)
+            .collect()
     }
 
+    /// Checks if `self` is in the rectangle specified by the opposite
+    /// corners `p_min` and `p_max`.
+    ///
+    /// *Note:* The bound made by `p_min` is inclusive  and the bound
+    /// made by `p_max` is exclusive
     pub fn is_in_rect(&self, p_min: Point, p_max: Point) -> bool {
         p_min.0 <= self.0 && self.0 < p_max.0 &&
             p_min.1 <= self.1 && self.1 < p_max.1
@@ -42,14 +69,6 @@ impl Add for Point {
 
     fn add(self, other: Self) -> Self {
         Self(self.0 + other.0, self.1 + other.1)
-    }
-}
-
-impl Sub for Point {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        Self(self.0 - other.0, self.1 - other.1)
     }
 }
 
@@ -74,17 +93,27 @@ mod tests {
             p + q == q + p
         }
 
-        fn prop_sub_pts(p: super::Point, q: super::Point) -> bool {
-            p + q - q == p
+        fn prop_full_circle(p: super::Point) -> bool {
+            let clockwise = Some(p) == p
+                .up()
+                .map(|x| x.right()).flatten()
+                .map(|x| x.down()).flatten()
+                .map(|x| x.left()).flatten();
+            let counterclockwise = Some(p) == p
+                .right()
+                .map(|x| x.up()).flatten()
+                .map(|x| x.left()).flatten()
+                .map(|x| x.down()).flatten();
+
+            clockwise && counterclockwise
         }
 
-        fn prop_adjacent_pts(p: super::Point) -> bool {
-            let sum: super::Point = p
+        fn prop_neighbor_of_neighbor(p: super::Point) -> bool {
+            p
                 .get_adjacent_pts()
                 .iter()
-                .fold(super::Point(0, 0), |acc, &x| acc + x);
-
-            super::Point(p.0 * 4, p.1 * 4) == sum
+                .flat_map(|&x| x.get_adjacent_pts())
+                .any(|x| x == p)
         }
     }
 }
